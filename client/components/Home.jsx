12e28440917fe8736d610d81b16eb3e6a47fd26b
student <student@eda-bc-06.localdomain>
2020-11-08 17:06:17 +1300

    work on mobile app page, home page and form

--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -8,7 +8,7 @@ render() {
 
       <>
         <div className='welcomeDiv'>
-         <h1 className='welcome'>Groundbreaking and Award-Winning game and application Studio </h1>
+         <h1 className='welcome'>Award-Winning NZ Game and Mobile Application Studio </h1>
          <h2 className='welcomeH2'>One day in the near future...Hopefully</h2>
         </div>
       </>
